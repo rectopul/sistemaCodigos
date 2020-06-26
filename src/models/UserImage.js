5f741c549f6ead5c3c76c144bb9f1b090ec043dc
Rogério Bonfim <rogeriomateus@icloud.com>
2020-06-26 03:14:50 -0300

    app remote

--- src/models/UserImage.js
+++ src/models/UserImage.js
@@ -20,10 +20,9 @@ class UserImage extends Model {
                     beforeSave: async (file) => {
                         if (!file.url) {
                             if (process.env.DATABASE_URL) {
-                                file.url = `${process.env.APP_URL}/files/${file.key}`
-                            } else {
-                                //URLAPPREMOTE
                                 file.url = `${process.env.URLAPPREMOTE}/files/${file.key}`
+                            } else {
+                                file.url = `${process.env.APP_URL}/files/${file.key}`
                             }
 
                             file.url = file.url.replace(' ', '%20')
