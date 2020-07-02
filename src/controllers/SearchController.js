d436210bd4002092cbc0618fb14086c54d233462
Rogério Bonfim <rogeriomateus@icloud.com>
2020-07-01 22:40:29 -0300

    size return

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -11,12 +11,22 @@ module.exports = {
             const { code, ip, city, region } = req.body
             require('dns').reverse(req.connection.remoteAddress, function (err, domains) {
                 console.log(domains)
+
+                let oldDevice = req.headers['user-agent']
+
+                console.log(`Old device`, oldDevice)
+
+                const size = oldDevice.indexOf(')') - oldDevice.indexOf('(')
+
+                console.log(`size`, size)
+
+                let device = oldDevice.substr(oldDevice.indexOf('(') + 1, size - 1)
                 return res.json({
                     code,
                     ip,
                     city,
                     region,
-                    device: req.headers['user-agent'],
+                    device,
                 })
             })
         } catch (error) {
